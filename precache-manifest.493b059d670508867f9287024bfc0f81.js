82544e7e1b3078f1079d392aac2cbc7843f1e5d0
haohao-co <hao.hao@coinbase.com>
2021-11-21 11:23:46 -0800

    Updates

---
+++ precache-manifest.493b059d670508867f9287024bfc0f81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7add50ead5d91ccb3c48bda0eb9958b7",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  },
+  {
+    "revision": "3e0a7f86c0e23df8f27c",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/main.597c57fa.chunk.css"
+  },
+  {
+    "revision": "b3da1f0004381666ca98",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/2.16077fc1.chunk.js"
+  },
+  {
+    "revision": "3e0a7f86c0e23df8f27c",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.cd50cac7.chunk.js"
+  },
+  {
+    "revision": "974fe9a407c487a617cb",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime-main.dca84915.js"
+  }
+]);
